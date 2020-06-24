71a086af9bce66829a51410262cf58ad7631aaf8
Yubei Yang <58965389+ConyYang@users.noreply.github.com>
2020-06-24 17:53:35 +0800

    Change load picture file

--- loadPictures.py
+++ loadPictures.py
@@ -6,12 +6,13 @@ style_path = "test_pics/style.png"
 figure_content = cv2.imread(content_path)
 figure_style = cv2.imread(style_path)
 
-fig = plt.figure(figsize=(8,8))
-figures = [figure_content, figure_style]
-columns = 2
-rows = 1
-for i in range(1, columns* rows+1):
-    img = cv2.cvtColor(figures[i-1], cv2.COLOR_BGR2BGRA)
-    fig.add_subplot(rows, columns, i)
-    plt.imshow(img)
-plt.show()
+def show_test():
+    fig = plt.figure(figsize=(8,8))
+    figures = [figure_content, figure_style]
+    columns = 2
+    rows = 1
+    for i in range(1, columns* rows+1):
+        img = cv2.cvtColor(figures[i-1], cv2.COLOR_BGR2BGRA)
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(img)
+    plt.show()
